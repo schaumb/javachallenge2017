1570212dfde793218eca58e0948941fb43402a6c
Andras Kucsma <andras.kucsma@prezi.com>
2017-12-02 12:58:04 +0100

    Stay defending

--- elodonto/src/main/java/logic/BestOfEveryTimeLogic.java
+++ elodonto/src/main/java/logic/BestOfEveryTimeLogic.java
@@ -116,7 +116,11 @@ public class BestOfEveryTimeLogic implements ILogic {
                     if (army.getSize() < (shouldWeRun ? 5 : 10)) {
                         continue;
                     }
-                    new Move().setMoveFrom(ps.getPlanetID()).setMoveTo(planets.get(0).getPlanetID()).setArmySize(army.getSize()).sendWithCheck(gameState, OUR_TEAM);
+                    int armyToSend = army.getSize();
+                    if (!shouldWeRun && army.getSize() > 50) {
+                        armyToSend = army.getSize() - 5;
+                    }
+                    new Move().setMoveFrom(ps.getPlanetID()).setMoveTo(planets.get(0).getPlanetID()).setArmySize(armyToSend).sendWithCheck(gameState, OUR_TEAM);
                 }
             }
         }
